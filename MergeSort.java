efad526dcfcd8023f4339ee48b66623468c1d6cd
Yagyata <yagyatasharmaa@gmail.com>
2025-03-21 19:51:28 +0530

    [Yagyata] ADD: Merge Sort - Sort an array of book prices

---
+++ MergeSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    public static void main(String[] args) {
+        int[] price = {320, 210, 500, 280, 300, 420};
+        mergeSort(price, 0, price.length - 1);
+        System.out.println("Books prices in sorted order: ");
+        System.out.println(Arrays.toString(price));
+    }
+}
+
