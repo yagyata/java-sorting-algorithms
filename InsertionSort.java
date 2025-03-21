0d3c704670037ec37ba8046c676c59afde7b98ba
Yagyata <yagyatasharmaa@gmail.com>
2025-03-21 19:46:32 +0530

    [Yagyata] ADD: Insertion Sort - Sort Employee IDs

---
+++ InsertionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] empId = {110, 132, 101, 102, 112, 109};
+        insertionSort(empId);
+        System.out.println(Arrays.toString(empId));
+    }
+}
